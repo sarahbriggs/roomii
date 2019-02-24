f001c4d2976edafc9002b33d34bdcbf6fcbb3b51
Stephen Zhou <zz105@duke.edu>
2019-02-24 15:55:48 -0500

    adding attributes

--- creatin_tables.sql
+++ creatin_tables.sql
@@ -1,8 +1,46 @@
-CREATE TABLE users(netid text PRIMARY KEY, profpic text);
-CREATE TABLE ratings(netid text PRIMARY KEY, cleanliness real, friendliness real, conscientiousness real, self_report_accuracy real);
-CREATE TABLE review(reviewer_netid text, reviewed_netid text, review_text text, overall_rating real, cleanliness real, friendliness real, conscientiousness real, PRIMARY KEY(reviewer_netid,reviewed_netid));
-CREATE TABLE report(reporter_netid text, reported_netid text, reason text, PRIMARY KEY(reported_netid, reported_netid));
-CREATE TABLE recommend(recommender_netid text, recommendee_netid text, recommended_netid text, reason text, PRIMARY KEY(recommender_netid,recommendee_netid,recommended_netid));
-CREATE TABLE questions(qid integer PRIMARY KEY, question_content text);
-CREATE TABLE answer_text(qid integer, answer_id integer, answer_content text, PRIMARY KEY(qid,answer_id));
-CREATE TABLE answer(netid text, qid integer, answer_id integer, weight real, PRIMARY KEY(netid,qid));
+CREATE TABLE users(
+	netid text PRIMARY KEY, 
+	profpic text);
+CREATE TABLE ratings(
+	netid text PRIMARY KEY, 
+	cleanliness real, 
+	friendliness real, 
+	conscientiousness real, 
+	self_report_accuracy real,
+	number_of_reports integer);
+CREATE TABLE review(
+	reviewer_netid text, 
+	reviewed_netid text, 
+	review_text text, 
+	overall_rating NOT NULL real, 
+	cleanliness real, 
+	friendliness real, 
+	conscientiousness real, 
+	PRIMARY KEY(reviewer_netid, reviewed_netid));
+CREATE TABLE report(
+	reporter_netid text, 
+	reported_netid text,
+	reason text, 
+	PRIMARY KEY(reported_netid, reported_netid));
+CREATE TABLE recommend(
+	recommender_netid text, 
+	recommendee_netid text, 
+	recommended_netid text, 
+	reason text, 
+	PRIMARY KEY(recommender_netid,recommendee_netid,recommended_netid));
+-- recommender - A, recommendee - B, recommended C. So A recommends C to B
+CREATE TABLE questions(
+	qid integer PRIMARY KEY,
+	question_content text);
+CREATE TABLE answer_text(
+	qid integer, 
+	answer_id integer, 
+	answer_content text, 
+	PRIMARY KEY(qid, answer_id));
+-- choices related to a question
+CREATE TABLE answer(
+	netid text, 
+	qid integer, 
+	answer_id integer, 
+	weight real, 
+	PRIMARY KEY(netid, qid));
