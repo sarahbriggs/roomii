2429b6cfd3e14be9cad2082cc5cdfcd47c74f682
Ryan Ferner <rjf19@duke.edu>
2019-03-28 20:13:48 -0400

    testing out the frontend

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
+from roomii import useful_operations, useful_queries
+import sqlite3
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template("base.html")
+
+@app.route("/questions")
+def questions():
+	conn = sqlite3.connect("fakedata.db")
+	question = useful_queries.get_question_text(conn,0)
+	return render_template("questions.html", question = question)
 
 app.run()
