8efd351f50f9372107edf3071d3bc8dfebbd7817
Sarah <seb103@duke.edu>
2019-04-13 23:51:37 -0400

    using materialize

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_login import LoginManager
+from flask_materialize import Material  
 import db_related.useful_operations as uo
 import db_related.useful_queries as uq
 import db_related.cheapSecurity as sec
@@ -7,11 +8,16 @@ import sqlite3
 
 app = Flask(__name__)
 login = LoginManager(app)
+Material(app)
 
 @app.route("/")
 def hello():
     return render_template("base.html")
 
+@app.route("/register")
+def register():
+	conn = sqlite3.connect("db_related/fakedata.db")
+	return render_template("register.html")
 
 @app.route("/questions")
 def questions():
