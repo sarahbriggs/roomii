14dce77aaff8e9a81d0a9eb25fee052bd8b77965
Ryan Ferner <rjf19@duke.edu>
2019-04-26 18:21:42 -0400

    by default get matchups returns only 20

--- app/db_related/useful_operations.py
+++ app/db_related/useful_operations.py
@@ -202,11 +202,11 @@ def all_matchups(conn,netid):
 	conn.commit()
 	return tk
 
-def get_matchups(conn, netid):
+def get_matchups(conn, netid, num = 20):
 	numMatches = all_matchups(conn, netid)
-	tup = (netid,)
+	tup = (num, netid,)
 	cursor = conn.cursor()
-	query = "SELECT * FROM matchups WHERE netid1 = ? ORDER BY matchRating DESC, netid2 ASC;"
+	query = "SELECT TOP ? * FROM matchups WHERE netid1 = ? ORDER BY matchRating DESC, netid2 ASC;"
 	cursor.execute(query, tup)
 	return cursor.fetchall()
 
