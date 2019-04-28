7c522d95c6434b486557e162bf0d4d884caae70b
Stephen Zhou <zz105@duke.edu>
2019-04-28 19:22:05 -0400

    change search form method to get

--- app/main.py
+++ app/main.py
@@ -260,68 +260,72 @@ def report():
 def searchUser():
 	if not loggedIn: 
 		return render_template("index.html", display_error = 2)
-	conn = sqlite3.connect("db_related/fakedata.db")
-	searchedNetid = request.form['netid']
-	searchedNetid = searchedNetid.upper()
-	if (searchedNetid == currentNetid):
-		return redirect(url_for("homepage"))
-	info = uq.get_user_info_friends(conn, searchedNetid)
-	rating = uq.get_user_rating(conn, searchedNetid)
-	overall = None
-	clean = None
-	friendly = None
-	consc = None
-	self_accuracy = None
-	num_reports = 0
-	if (rating != False):
-		overall = rating[1]
-		clean = rating[2]
-		friendly = rating[3]
-		consc = rating[4]
-		self_accuracy = rating[5]
-		num_reports = rating[6]
-	if (info != False):
-		given_name = info[1]
-		family_name = info[2]
-		profpic = info[3]
-		if profpic == "":
-			profpic = "http://friendoprod.blob.core.windows.net/missionpics/images/4846/member/f9d9c34c-d5c8-495a-bd84-45b693edf7a2.jpg" # pikachu photo
-		description = info[4]
-		phone = info[6]
-		email = info[7]
-		sourceNetid = currentNetid
-		areFriends = uq.are_friends(conn, searchedNetid, sourceNetid) or uq.are_friends(conn, sourceNetid, searchedNetid)
-		conn.close()
-		if areFriends:
-			return render_template("searchUser.html", 
-				searchedNetid = searchedNetid,
-				given_name = given_name,
-				family_name = family_name,
-				profpic = profpic,
-				description = description,
-				phone = phone,
-				email = email,
-				overall = overall,
-				clean = clean,
-				friendly = friendly,
-				consc = consc,
-				self_accuracy = self_accuracy,
-				num_reports = num_reports)
+	if (request.method == 'GET'):
+		conn = sqlite3.connect("db_related/fakedata.db")
+		searchedNetid = request.args.get('netid')
+		print(searchedNetid)
+		searchedNetid = searchedNetid.upper()
+		if (searchedNetid == currentNetid.upper()):
+			return redirect(url_for("homepage"))
+		info = uq.get_user_info_friends(conn, searchedNetid)
+		rating = uq.get_user_rating(conn, searchedNetid)
+		overall = None
+		clean = None
+		friendly = None
+		consc = None
+		self_accuracy = None
+		num_reports = 0
+		if (rating != False):
+			overall = rating[1]
+			clean = rating[2]
+			friendly = rating[3]
+			consc = rating[4]
+			self_accuracy = rating[5]
+			num_reports = rating[6]
+		if (info != False):
+			given_name = info[1]
+			family_name = info[2]
+			profpic = info[3]
+			if profpic == "":
+				profpic = "http://friendoprod.blob.core.windows.net/missionpics/images/4846/member/f9d9c34c-d5c8-495a-bd84-45b693edf7a2.jpg" # pikachu photo
+			description = info[4]
+			phone = info[6]
+			email = info[7]
+			sourceNetid = currentNetid
+			areFriends = uq.are_friends(conn, searchedNetid, sourceNetid) or uq.are_friends(conn, sourceNetid, searchedNetid)
+			conn.close()
+			if areFriends:
+				return render_template("searchUser.html", 
+					searchedNetid = searchedNetid,
+					given_name = given_name,
+					family_name = family_name,
+					profpic = profpic,
+					description = description,
+					phone = phone,
+					email = email,
+					overall = overall,
+					clean = clean,
+					friendly = friendly,
+					consc = consc,
+					self_accuracy = self_accuracy,
+					num_reports = num_reports)
+			else:
+				return render_template("searchUser.html", 
+					searchedNetid = searchedNetid,
+					given_name = given_name,
+					family_name = family_name,
+					profpic = profpic,
+					description = description,
+					phone = None,
+					email = None,
+					overall = overall,
+					clean = clean,
+					friendly = friendly,
+					consc = consc,
+					self_accuracy = self_accuracy,
+					num_reports = num_reports)
 		else:
-			return render_template("searchUser.html", 
-				searchedNetid = searchedNetid,
-				given_name = given_name,
-				family_name = family_name,
-				profpic = profpic,
-				description = description,
-				phone = None,
-				email = None,
-				overall = overall,
-				clean = clean,
-				friendly = friendly,
-				consc = consc,
-				self_accuracy = self_accuracy,
-				num_reports = num_reports)
+			return render_template("notExist.html")
 	else:
 		return render_template("notExist.html")
 
